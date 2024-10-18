f81db1b3c2ddc6dcec446de752c42544546eea89
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-10-18 17:26:51 +0330

    implement dependency injection project in infrastructure layer of shopManagement

---
+++ ShopManagement.infrastructure.Configuration/ShopManagementDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application;
+using ShopManagement.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EfCore;
+using ShopManagement.Infrastructure.EfCore.Repository;
+
+namespace ShopManagement.infrastructure.Configuration;
+
+public class ShopManagementDependencyInjection
+{
+    public static void Configuration(IServiceCollection services, string connectionString)
+    {
+        services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+        services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+    }
+}

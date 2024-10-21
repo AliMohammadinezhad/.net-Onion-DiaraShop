ba206eccce02542383fd454ab48850b807c8bb77
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-10-21 19:14:08 +0330

    implement presentation layer

---
+++ ServiceHost/Areas/Administration/Pages/Shop/ProductPictures/Index.cshtml.cs
@@ -0,0 +1,78 @@
+using Framework.Application;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Contracts.Product;
+using ShopManagement.Contracts.ProductPicture;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.ProductPictures;
+    public class IndexModel : PageModel
+    {
+        [TempData] public string Message { get; set; }
+        public ProductPictureSearchModel SearchModel;
+        public SelectList Products;
+        public List<ProductPictureViewModel> ProductPictures;
+
+        private readonly IProductPictureApplication _productPictureApplication;
+        private readonly IProductApplication _productApplication;
+
+        public IndexModel(IProductPictureApplication productPictureApplication, IProductApplication productApplication)
+        {
+            _productPictureApplication = productPictureApplication;
+            _productApplication = productApplication;
+        }
+
+
+        public void OnGet(ProductPictureSearchModel searchModel)
+        {
+            Products = new SelectList(_productApplication.GetProducts(), "Id", "Name");
+            ProductPictures = _productPictureApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            var command = new CreateProductPicture
+            {
+                Products = _productApplication.GetProducts()
+            };
+            return Partial("./Create", command);
+        }
+
+        public IActionResult OnPostCreate(CreateProductPicture command)
+        {
+            var result = _productPictureApplication.Create(command);
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(long id)
+        {
+            var productPicture = _productPictureApplication.GetDetails(id);
+            productPicture.Products = _productApplication.GetProducts();
+            return Partial("./Edit", productPicture);
+        }
+
+        public JsonResult OnPostEdit(EditProductPicture command)
+        {
+            var result = _productPictureApplication.Edit(command);
+            return new JsonResult(result);
+        }
+
+        public RedirectToPageResult OnGetRemoved(long id)
+        {
+            OperationResult result = _productPictureApplication.Remove(id);
+            if (result.IsSucceeded)
+                return RedirectToPage("./Index");
+            Message = result.Message;
+            return RedirectToPage("./Index");
+        }
+
+        public RedirectToPageResult OnGetRestore(long id)
+        {
+            OperationResult result = _productPictureApplication.Restore(id);
+            if (result.IsSucceeded)
+                return RedirectToPage("./Index");
+            Message = result.Message;
+            return RedirectToPage("./Index");
+        }
+    } 
+

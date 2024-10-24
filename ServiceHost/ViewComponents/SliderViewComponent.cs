bfbae344551988374e36090b4bd637783f4c3b63
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-10-24 13:27:21 +0330

    update UI to show slider from admin panel

---
+++ ServiceHost/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Query.Contracts.Slide;
+
+namespace ServiceHost.ViewComponents;
+
+public class SliderViewComponent : ViewComponent
+{
+    private readonly ISlideQuery _slideQuery;
+
+    public SliderViewComponent(ISlideQuery slideQuery)
+    {
+        _slideQuery = slideQuery;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var slides = _slideQuery.GetSlides();
+        return View(slides);
+    }
+}

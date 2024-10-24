0967dbbb7524436a5c65b85628174ca55fa54720
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-10-24 15:50:02 +0330

    Create domain and application contract of  discount management

---
+++ DiscountManagement.Application.Contract/CustomerDiscount/ICustomerApplication.cs
@@ -0,0 +1,11 @@
+﻿using Framework.Application;
+
+namespace DiscountManagement.Application.Contract.CustomerDiscount;
+
+public interface ICustomerApplication
+{
+    OperationResult Define(DefineCustomerDiscount command);
+    OperationResult Edit(EditCustomerDiscount command);
+    EditCustomerDiscount GetDetails(long id);
+    List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel);
+}
---
+++ DiscountManagement.Domain/CustomerDiscountAgg/ICustomerDiscountRepository.cs
@@ -0,0 +1,10 @@
+﻿using DiscountManagement.Application.Contract.CustomerDiscount;
+using Framework.Domain;
+
+namespace DiscountManagement.Domain.CustomerDiscountAgg;
+
+public interface ICustomerDiscountRepository : IRepository<long, CustomerDiscount>
+{
+    EditCustomerDiscount GetDetails(long id);
+    List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel);
+}

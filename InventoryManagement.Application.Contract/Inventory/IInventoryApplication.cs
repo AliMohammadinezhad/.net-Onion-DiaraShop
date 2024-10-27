a4f0effd65654bb9d35889fd61d654ea25f04024
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-10-27 18:58:00 +0330

    implement application contract and interface of repository

---
+++ InventoryManagement.Application.Contract/Inventory/IInventoryApplication.cs
@@ -0,0 +1,14 @@
+﻿using Framework.Application;
+
+namespace InventoryManagement.Application.Contract.Inventory;
+
+public interface IInventoryApplication
+{
+    OperationResult Create(CreateInventory command);
+    OperationResult Edit(EditInventory command);
+    OperationResult Increase(IncreaseInventory command);
+    OperationResult Decrease(DecreaseInventory command);
+    OperationResult Decrease(List<DecreaseInventory> command);
+    EditInventory GetDetails(long id);
+    List<InventoryViewModel> Search(InventorySearchModel searchModel);
+}

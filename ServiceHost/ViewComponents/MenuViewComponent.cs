2c785caefd4007cb68fb19d089068ec959ac6bf6
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-11-08 15:12:23 +0330

    move menu and footer into new view components

---
+++ ServiceHost/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Query.Contracts.ProductCategory;
+
+namespace ServiceHost.ViewComponents;
+
+public class MenuViewComponent : ViewComponent
+{
+    private readonly IProductCategoryQuery _productCategory;
+
+    public MenuViewComponent(IProductCategoryQuery productCategory)
+    {
+        _productCategory = productCategory;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}

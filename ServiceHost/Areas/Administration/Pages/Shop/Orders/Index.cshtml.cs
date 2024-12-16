6635d3fb55c3ab4335c1cd69a9df1badf969e227
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-12-17 01:28:23 +0330

    implement UI admin part of Order system

---
+++ ServiceHost/Areas/Administration/Pages/Shop/Orders/Index.cshtml.cs
@@ -0,0 +1,52 @@
+using AccountManagement.Application.Contract.Account;
+using Framework.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Contracts.Order;
+using ShopManagement.Contracts.ProductCategory;
+using ShopManagement.infrastructure.Configuration.Permissions;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.Orders
+{
+    public class IndexModel : PageModel
+    {
+        public OrderSearchModel SearchModel;
+        public SelectList Accounts;
+        public List<OrderViewModel> Order;
+
+        private readonly IOrderApplication _orderApplication;
+        private readonly IAccountApplication _accountApplication;
+        public IndexModel(IOrderApplication orderApplication, IAccountApplication accountApplication)
+        {
+            _orderApplication = orderApplication;
+            _accountApplication = accountApplication;
+        }
+
+        [NeedsPermission(ShopPermissions.ListOrders)]
+        public void OnGet(OrderSearchModel searchModel)
+        {
+            Accounts = new SelectList(_accountApplication.GetAccounts(), "Id", "FullName");
+            Order = _orderApplication.Search(searchModel);
+        }
+
+        [NeedsPermission(ShopPermissions.ConfirmOrder)]
+        public IActionResult OnGetConfirm(long id)
+        {
+            _orderApplication.PaymentSucceeded(id, 0);
+            return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnGetCancel(long id)
+        {
+            _orderApplication.Cancel(id);
+            return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnGetItems(long id)
+        {
+            var items = _orderApplication.GetItems(id);
+            return Partial("Items", items);
+        }
+    }
+}
